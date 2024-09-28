b97877217e9ef97cae08958229d014ed79577b8e
global <global@example.com>
2024-09-28 20:33:03 +0900

    feature/v0_root_and_bag

--- src/contents/root.js
+++ src/contents/root.js
@@ -8,8 +8,15 @@ function getOption(key) {
 function setOption(key, value) {
   return browser_or_chrome.runtime.sendMessage({setOption: {key: key, value: value}});
 }
+function isLogin(doc = document) {
+  return doc.querySelector('body > h1').textContent == 'どんぐり基地';
+}
+function isGuard(doc = document) {
+  return doc.querySelector('div.stats > div > div:nth-child(2)').textContent.startsWith('警備員');
+}
 
 getOption('enabled_root_auto_access').then(enabled => { if(enabled == 'true') {
+  if (!isLogin()) return;
   const logTag = document.createElement('textarea');
   logTag.setAttribute('readonly', '');
   logTag.style.resize = 'none';
@@ -37,6 +44,7 @@ getOption('enabled_root_auto_access').then(enabled => { if(enabled == 'true') {
 }}).catch(err => {});
 
 getOption('enabled_log_filter_self').then(enabled => { if(enabled == 'true') {
+  if (!isLogin()) return;
   const tag = document.querySelectorAll('div.stats > div > div');  
   const name = tag[0].textContent;
   const id = tag[1].textContent.split('[ID: ')[1].slice(0, -1);
@@ -51,6 +59,35 @@ getOption('enabled_log_filter_self').then(enabled => { if(enabled == 'true') {
   });
 }}).catch(err => {});
 
+getOption('enabled_root_lvup_disp').then(enabled => { if(enabled == 'true') {
+  if (!isLogin()) return;
+  function getDateString(level, e) {
+    const f = lv => (lv / 0.196) ** 2;
+    const time = Math.ceil(((f(level[0] + 1) - f(level[0])) * e) * (101 - level[1]) * 1000 / 101);
+    return new Date(Date.now() + time).toLocaleString('sv-SE');
+  }
+  const guard = isGuard();
+  Array.from(document.querySelectorAll('div.stat-block:nth-child(2) > div')).forEach((tag, index) => {
+    if (index == 0) {
+      const level = Array.from(tag.childNodes).map(node=>parseInt(/\d+/.exec(node.textContent)));
+      const bar = tag.lastChild.lastChild;
+      bar.style.textAlign = 'left';
+      bar.style.whiteSpace = 'pre';
+      bar.append(`（${getDateString(level, guard ? 10 : 0.5)}）`);
+    } else if (index == 1) {
+      //const level = Array.from(tag.childNodes).map(node=>parseInt(/\d+/.exec(node.textContent)));
+      const bar = tag.lastChild.lastChild;
+      bar.style.textAlign = 'left';
+      bar.style.whiteSpace = 'pre';
+    } else {
+      const level = Array.from(tag.children).map(element=>parseInt(/\d+/.exec(element.textContent)));
+      const bar = tag.lastChild.lastChild;
+      bar.style.textAlign = 'left';
+      bar.style.whiteSpace = 'pre';
+      bar.append(`（${getDateString(level, guard ? 10: 1)}）`);
+    }
+  });
+}}).catch(err => {});
 
 
 
